85c9d4662e6f061126ca76a023765f60b44a6422
Daniel Kish <aox.soljah@gmail.com>
2022-03-02 06:47:14 +0700

    more setup

---
+++ api/posts/posts-router.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const axios = require('axios')
+const router = express.Router()
+
+
+router.get('/', async (req, res, next) => {
+    try {
+        const test =
+    } catch (error) {
+        
+    }
+})
+
+module.exports = router
